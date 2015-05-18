7a857e1c1cc94410f6db9eb5965c6a5a7bdb3151
Rob Thijssen <rthijssen@gmail.com>
2015-05-18 11:51:44 +0100

    yo angular scaffolding

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yogaloftNgApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yogaloftNgApp
+ */
+angular.module('yogaloftNgApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
