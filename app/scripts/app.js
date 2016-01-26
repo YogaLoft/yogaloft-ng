8aed5e79ff7585401de9cc618ad6b5b2c16f0aa0
Rob Thijssen <rthijssen@gmail.com>
2016-01-26 16:09:02 +0000

    moved fb init to app.run()

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,6 +19,26 @@ angular
     'ngIcal',
     'hc.marked'
   ])
+  .run(function($window) {
+    $window.fbAsyncInit = function() {
+      FB.init({
+        appId: '1434342883499221',
+        status: true,
+        cookie: true,
+        xfbml: true,
+        version: 'v2.5'
+      });
+    };
+    (function(d, id) {
+      var js, ref = d.getElementsByTagName('script')[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement('script');
+      js.id = id;
+      js.async = true;
+      js.src = '//connect.facebook.net/en_GB/sdk.js';
+      ref.parentNode.insertBefore(js, ref);
+    }(document, 'facebook-jssdk'));
+  })
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
