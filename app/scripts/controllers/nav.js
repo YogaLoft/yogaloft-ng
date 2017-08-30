cea9d9d9ec4ad168773781bb46f36e49425d8eba
Rob Thijssen <rthijssen@gmail.com>
2017-08-30 11:50:49 +0300

    Revert "no nav necessary"
    This reverts commit b7f6213789238fa5b7302973d0d8a8242ebc9718.

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,1 +1,24 @@
+'use strict';
 
+/**
+ * @ngdoc function
+ * @name ylngApp.controller:NavCtrl
+ * @description
+ * # NavCtrl
+ * Controller of the ylngApp
+ */
+angular.module('ylngApp')
+  .controller('NavCtrl', function ($scope, $location, classCalendar) {
+    
+    $scope.isActive = function (loc) {
+      return (($location.path() === '/' && loc === '/') || ($location.path() !== '/' && loc !== '/' && $location.path().startsWith(loc)));
+    };
+    classCalendar.index().then(
+      function (ci) {
+        $scope.ci = ci;
+      },
+      function (error) {
+        console.log(error.statusText);
+      }
+    );
+  });
